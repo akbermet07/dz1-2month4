f5d51485e453bb7fba5895cf61e44efa8dfcdea3
“akbermet07” <taalaibekkyzyakbermet@gmail.com>
2024-01-20 22:49:21 +0600

    Initial Commit

---
+++ dz1month4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dz1month4
+//
+//  Created by Акбермет Таалайбек кызы on 20/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
